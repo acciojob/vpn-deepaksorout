ab2b9431e729502208c4220a2299701978f85a4e
deepaksorout <39903636+deepaksorout@users.noreply.github.com>
2023-03-04 17:20:23 +0530

    f

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -7,11 +7,10 @@ import com.driver.model.ServiceProvider;
 import com.driver.repository.AdminRepository;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
+import com.driver.repository.UserRepository;
 import com.driver.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class AdminServiceImpl implements AdminService {
@@ -23,69 +22,60 @@ public class AdminServiceImpl implements AdminService {
 
     @Autowired
     CountryRepository countryRepository1;
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public Admin register(String username, String password) {
-        Admin admin=new Admin();
-
-        //setting up admin attributes
-        admin.setPassword(password);
+        Admin admin = new Admin();
         admin.setUsername(username);
+        admin.setPassword(password);
 
         adminRepository1.save(admin);
-
         return admin;
     }
 
     @Override
     public Admin addServiceProvider(int adminId, String providerName) {
-        ServiceProvider serviceProvider=new ServiceProvider();
-        Admin admin=adminRepository1.findById(adminId).get();
-        serviceProvider.setName(providerName);
+        Admin admin = adminRepository1.findById(adminId).get();
+        ServiceProvider serviceProvider = new ServiceProvider();
+
         serviceProvider.setAdmin(admin);
-
-        List<ServiceProvider>serviceProviderList=admin.getServiceProviders();
-        serviceProviderList.add(serviceProvider);
-        admin.setServiceProviders(serviceProviderList);
-
+        serviceProvider.setName(providerName);
+        admin.getServiceProviders().add(serviceProvider);
         adminRepository1.save(admin);
-
         return admin;
     }
 
     @Override
     public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
-        Country country=new Country();
-        if(countryName.equalsIgnoreCase("IND") || countryName.equalsIgnoreCase("USA")|| countryName.equalsIgnoreCase("JPN") || countryName.equalsIgnoreCase("CHI")||countryName.equalsIgnoreCase("AUS")) {
-            if (countryName.equalsIgnoreCase("IND")) {
+        if(countryName.equalsIgnoreCase("IND") || countryName.equalsIgnoreCase("USA") || countryName.equalsIgnoreCase("JPN") || countryName.equalsIgnoreCase("CHI") || countryName.equalsIgnoreCase("AUS")){
+            Country country = new Country();
+
+            ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
+
+            if(countryName.equalsIgnoreCase("IND")){
                 country.setCountryName(CountryName.IND);
                 country.setCode(CountryName.IND.toCode());
             }
-            if (countryName.equalsIgnoreCase("USA")) {
+            if(countryName.equalsIgnoreCase("USA")){
                 country.setCountryName(CountryName.USA);
                 country.setCode(CountryName.USA.toCode());
             }
-            if (countryName.equalsIgnoreCase("JPN")) {
+            if(countryName.equalsIgnoreCase("JPN")){
                 country.setCountryName(CountryName.JPN);
                 country.setCode(CountryName.JPN.toCode());
             }
-            if (countryName.equalsIgnoreCase("CHI")) {
+            if(countryName.equalsIgnoreCase("CHI")){
                 country.setCountryName(CountryName.CHI);
                 country.setCode(CountryName.CHI.toCode());
             }
-            if (countryName.equalsIgnoreCase("AUS")) {
+            if(countryName.equalsIgnoreCase("AUS")){
                 country.setCountryName(CountryName.AUS);
                 country.setCode(CountryName.AUS.toCode());
             }
-
-            ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
             country.setServiceProvider(serviceProvider);
-            country.setUser(null);
-
-            List<Country> countryList = serviceProvider.getCountryList();
-            countryList.add(country);
-            serviceProvider.setCountryList(countryList);
-
+            serviceProvider.getCountryList().add(country);
             serviceProviderRepository1.save(serviceProvider);
 
             return serviceProvider;
